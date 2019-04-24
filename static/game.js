de4516f15eb01d57ddaa3ba0ec7c9214323adb32
Collin <mcnultyc@allegheny.edu>
2019-04-24 18:26:51 -0400

    updates

--- static/game.js
+++ static/game.js
@@ -3,6 +3,8 @@ const FPS = 30; //frame per second
 const SHIP_SIZE = 30; //ship height in pixels
 const SHIP_THRUST = 5; //acceleration of ship in pixels per second per second
 
+
+
 socket.on('message', function(data) {
   console.log(data);
 });
@@ -11,7 +13,8 @@ var movement = {
   up: false,
   down: false,
   left: false,
-  right: false
+  right: false,
+  shoot: false
 }
 document.addEventListener('keydown', function(event) {
   switch (event.keyCode) {
@@ -23,6 +26,9 @@ document.addEventListener('keydown', function(event) {
       break;
     case 87: // W
       movement.up = true;
+      break;
+    case 32: //space
+      movement.shoot = true;
       break;
   }
 });
@@ -37,12 +43,17 @@ document.addEventListener('keyup', function(event) {
     case 87: // W
       movement.up = false;
       break;
+    case 32: //space
+      movement.shoot = false;
+      break;
   }
 });
 
 socket.emit('new player');
+
 setInterval(function() {
   socket.emit('movement', movement);
+  //screen wrap
 }, 1000 / 60);
 
 
@@ -51,13 +62,13 @@ var canvas = document.getElementById('canvas');
 canvas.width = 800;
 canvas.height = 600;
 var context = canvas.getContext('2d');
-socket.on('state', function(players) {
+socket.on('state', function(world) {
   context.clearRect(0, 0, 800, 600);
   context.fillStyle = 'green';
   context.strokeStyle = "black";
   context.lineWidth =  SHIP_SIZE / 20;
-  for (var id in players) {
-    var player = players[id];
+  for (var id in world.players) {
+    var player = world.players[id];
     context.beginPath();
     context.moveTo( //nose of the ship
       player.x + 4/3 * player.r * Math.cos(player.a),
@@ -77,4 +88,29 @@ socket.on('state', function(players) {
     context.fillStyle = "blue";
     context.fill();
   }
+//  for (var id in world.bullets)
+// draw the lasers
+for (var id in world.players.lasers) {
+          ctx.fillStyle = "salmon";
+          ctx.beginPath();
+          ctx.arc(player.lasers[i].x, player.lasers[i].y, SHIP_SIZE / 15, 0, Math.PI * 2, false);
+          ctx.fill();
+        }
+        //else {
+// draw the eplosion
+//          ctx.fillStyle = "orangered";
+//          ctx.beginPath();
+//          ctx.arc(player.lasers[i].x, player.lasers[i].y, player.r * 0.75, 0, Math.PI * 2, false);
+//          ctx.fill();
+//          ctx.fillStyle = "salmon";
+//          ctx.beginPath();
+//          ctx.arc(player.lasers[i].x, player.lasers[i].y, player.r * 0.5, 0, Math.PI * 2, false);
+//          ctx.fill();
+//          ctx.fillStyle = "pink";
+//          ctx.beginPath();
+//          ctx.arc(player.lasers[i].x, player.lasers[i].y, player.r * 0.25, 0, Math.PI * 2, false);
+//          ctx.fill();
+              //  }
+          //  }
+        //  }
 });
