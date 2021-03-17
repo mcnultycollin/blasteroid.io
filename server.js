0cc4bd3df3abe52fc5a0d8bcf34ffe24f3eace70
Collin McNulty <mcnultyc@allegheny.edu>
2021-03-17 17:21:04 -0400

    adds proccess.env.port

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var socketIO = require('socket.io');
 var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
-app.set('port', 3000);
+app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));
 // Routing
 app.get('/', function(request, response) {
@@ -30,8 +30,8 @@ app.get('/demo', function(request, response) {
   response.sendFile(path.join(__dirname, 'Blasteroid.html'));
 });
 // Starts the server.
-server.listen(3000, function() {
-  console.log('Starting server on port 3000');
+server.listen(process.env.PORT || 5000, function() {
+  console.log('Starting server on port 5000');
 });
 
 // Add the WebSocket handlers
